05caf5fd74ccfca93ba105810501cf9d9ee52b80
Pavel <pavel2001stepanov@yandex.ru>
2024-04-21 18:48:14 +0300

    add Head - 1

---
+++ src/firstProgrammJava/HelloDate.java
@@ -0,0 +1,12 @@
+package firstProgrammJava;
+
+import java.util.Date;
+
+public class HelloDate {
+
+	public static void main(String[] args) {
+		System.out.println("Какой сегодня день?");
+		System.out.println(new Date());
+	}
+
+}
