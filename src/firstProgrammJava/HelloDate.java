8d99ba782bb3e287ac98755ecd075ac5569c5d0e
Pavel <pavel2001stepanov@yandex.ru>
2024-04-28 19:31:32 +0300

    add streams

--- src/firstProgrammJava/HelloDate.java
+++ src/firstProgrammJava/HelloDate.java
@@ -1,12 +1,57 @@
 package firstProgrammJava;
 
-import java.util.Date;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class HelloDate {
 
-	public static void main(String[] args) {
-		System.out.println("Какой сегодня день?");
-		System.out.println(new Date());
+	private static List<Double> movingAverage(int n, List<Integer> arr, int windowSize) {
+		// ваше решение
+		List<Double> result = new ArrayList<>();
+		int sum = 0;
+
+		for (int i = 0; i < windowSize; i++) {
+			sum += arr.get(i);
+		}
+		result.add((double) sum / windowSize);
+
+		for (int i = windowSize; i < n; i++) {
+			sum += arr.get(i) - arr.get(i - windowSize);
+			result.add((double) sum / windowSize);
+		}
+
+		return result;
+
 	}
 
+	public static void main(String[] args) throws IOException {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
+			int n = readInt(reader);
+			List<Integer> arr = readList(reader);
+			int windowSize = readInt(reader);
+			List<Double> result = movingAverage(n, arr, windowSize);
+			for (double elem : result) {
+				writer.write(elem + " ");
+			}
+		}
+	}
+
+	private static int readInt(BufferedReader reader) throws IOException {
+		return Integer.parseInt(reader.readLine());
+	}
+
+	private static List<Integer> readList(BufferedReader reader) throws IOException {
+		return Arrays.asList(reader.readLine().split(" ")).stream().map(elem -> Integer.parseInt(elem))
+				.collect(Collectors.toList());
+	}
 }
